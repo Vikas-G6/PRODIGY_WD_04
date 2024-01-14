bc2ea65d19af2fb21e3600afa533f51b651b3612
Vikas-G6 <117025250+Vikas-G6@users.noreply.github.com>
2024-01-14 21:50:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// JavaScript for Form Submission
+function submitForm() {
+    alert('Form submitted!');
+    document.getElementById('contactForm').reset();
+}
